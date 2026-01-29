ddac27592b6808e009dadb9d252b868182df4b53
Asztalos-Kiss Alexandra <asztalos-kissalexandra@Asztalos-Kiss-Laptop.local>
2026-01-29 09:32:44 +0100

    Initial SwiftUI app setup

---
+++ Sources/Your3dPrintDecisionBuddyApp.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import AppKit
+
+@main
+struct Your3dPrintDecisionBuddyApp: App {
+    init() {
+        // Required for swift run to show GUI window
+        NSApplication.shared.setActivationPolicy(.regular)
+        NSApplication.shared.activate(ignoringOtherApps: true)
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+        .defaultSize(width: 800, height: 600)
+    }
+}
